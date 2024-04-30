d3e9da53eab306a9285627c054ed68732e13c2ac
Kaburi <specialemail90@gmail.com>
2024-04-30 15:32:57 +0900

    Updated

--- src/joi/index.ts
+++ src/joi/index.ts
@@ -1,7 +1,6 @@
-import Joi from "joi";
-export * from "./activity";
-export * from "./drink";
-export * from "./history";
-export * from "./smoking";
+// export * from "./activity";
+// export * from "./drink";
+// export * from "./history";
+// export * from "./smoking";
 export * from "./questionnaire";
 
--- src/joi/questionnaire.ts
+++ src/joi/questionnaire.ts
@@ -1,13 +1,17 @@
 import Joi from "joi";
-import { IQuestionnaire } from "../interfaces/questionnaire";
-import { HistorySchema } from "./history";
-import { SmokingSchema } from "./smoking";
-import { DrinkSchema } from "./drink";
-import { ActivitySchema } from "./activity";
+// import { IQuestionnaire } from "../interfaces/questionnaire";
+// import { HistorySchema } from "./history";
+// import { SmokingSchema } from "./smoking";
+// import { DrinkSchema } from "./drink";
+// import { ActivitySchema } from "./activity";
 
-export const QuestionnaireSchema = Joi.object<IQuestionnaire>({
-  history: HistorySchema.required(),
-  smoking: SmokingSchema.required(),
-  drink: DrinkSchema.required(),
-  activity: ActivitySchema.required(),
+// export const QuestionnaireSchema = Joi.object<IQuestionnaire>({
+//   history: HistorySchema.required(),
+//   smoking: SmokingSchema.required(),
+//   drink: DrinkSchema.required(),
+//   activity: ActivitySchema.required(),
+// });
+
+export const schema = Joi.object({
+  history: Joi.string(),
 });
