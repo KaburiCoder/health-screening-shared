fd1b2d149cff14ac47533999f9bad7e0fa5a5d16
Kaburi <specialemail90@gmail.com>
2024-04-30 17:07:55 +0900

    Updated

--- src/interfaces/socket/questionnaire.ts
+++ src/interfaces/socket/questionnaire.ts
@@ -3,6 +3,7 @@ import { EmitArgsBase, EmitResultBase } from "./base";
 
 export interface SaveQuestionnaireArgs extends EmitArgsBase, IQuestionnaire {
   eiAuto: number;
+  error: { [key: string]: string };
 }
 
 export interface SaveQuestionnaireResult extends EmitResultBase<number> {}
