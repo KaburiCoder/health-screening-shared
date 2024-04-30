d3089ea9949b5f5081e2a66a43a155d5db35a73e
Kaburi <specialemail90@gmail.com>
2024-04-30 16:10:41 +0900

    Updated

--- src/joi/index.ts
+++ src/joi/index.ts
@@ -1,6 +1,6 @@
 export * from "./activity";
 // export * from "./drink";
-// export * from "./history";
+export * from "./history";
 // export * from "./smoking";
 // export * from "./questionnaire";
 
