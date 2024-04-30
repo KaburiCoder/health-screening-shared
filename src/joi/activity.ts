f128e2b333a13b1bfeaa92cfbde14946d0a8edb1
Kaburi <specialemail90@gmail.com>
2024-04-30 16:11:24 +0900

    Updated

--- src/joi/activity.ts
+++ src/joi/activity.ts
@@ -1,13 +1,14 @@
 import Joi from "joi";
+import { IActivity, IActivityTerm } from "../interfaces/index";
 
-export const ActivitySchema = Joi.object({
+export const ActivitySchema = Joi.object<IActivity>({
   n8_1: Joi.number().min(1).max(7).required(),
-  n8_2: Joi.object({
+  n8_2: Joi.object<IActivityTerm>({
     hours: Joi.number().min(1).max(23).required(),
     minutes: Joi.number().min(0).max(59).default(0).required(),
   }).required(),
   n9_1: Joi.number().min(1).max(7).required(),
-  n9_2: Joi.object({
+  n9_2: Joi.object<IActivityTerm>({
     hours: Joi.number().min(1).max(23).required(),
     minutes: Joi.number().min(0).max(59).default(0).required(),
   }).required(),
