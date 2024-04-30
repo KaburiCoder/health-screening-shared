7286366773ba1b3c4d92593359de489b77bed9ef
Kaburi <specialemail90@gmail.com>
2024-04-30 16:01:19 +0900

    Updated

--- src/joi/activity.ts
+++ src/joi/activity.ts
@@ -1,20 +1,14 @@
 import Joi from "joi";
 
-// const ActiveTermSchema = Joi.object<IActivityTerm>({
-//   hours: Joi.number().min(1).max(23).required(),
-//   minutes: Joi.number().min(0).max(59).default(0).required(),
-// });
+const ActiveTermSchema = Joi.object({
+  hours: Joi.number().min(1).max(23).required(),
+  minutes: Joi.number().min(0).max(59).default(0).required(),
+});
 
 export const ActivitySchema = Joi.object({
   n8_1: Joi.number().min(1).max(7).required(),
-  n8_2: Joi.object({
-    hours: Joi.number().min(1).max(23).required(),
-    minutes: Joi.number().min(0).max(59).default(0).required(),
-  }).required(),
+  n8_2: ActiveTermSchema.required(),
   n9_1: Joi.number().min(1).max(7).required(),
-  n9_2: Joi.object({
-    hours: Joi.number().min(1).max(23).required(),
-    minutes: Joi.number().min(0).max(59).default(0).required(),
-  }).required(),
+  n9_2: ActiveTermSchema.required(),
   n10: Joi.number().required(),
 });
