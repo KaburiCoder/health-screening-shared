2ff3f7d517a64dc18e40953f0cd12e32f04efcd3
Kaburi <specialemail90@gmail.com>
2024-04-30 15:52:45 +0900

    Updated

--- src/joi/questionnaire.ts
+++ src/joi/questionnaire.ts
@@ -1,14 +1,13 @@
 import Joi from "joi";
 import { IQuestionnaire } from "../interfaces/questionnaire";
 import { HistorySchema } from "./history";
-// import { SmokingSchema } from "./smoking";
-// import { DrinkSchema } from "./drink";
-// import { ActivitySchema } from "./activity";
+import { SmokingSchema } from "./smoking";
+import { DrinkSchema } from "./drink";
+import { ActivitySchema } from "./activity";
 
-export const his = HistorySchema;
 export const QuestionnaireSchema = Joi.object<IQuestionnaire>({
-  history: his.required(),
-  // smoking: SmokingSchema.required(),
-  // drink: DrinkSchema.required(),
-  // activity: ActivitySchema.required(),
+  history: HistorySchema.required(),
+  smoking: SmokingSchema.required(),
+  drink: DrinkSchema.required(),
+  activity: ActivitySchema.required(),
 });
