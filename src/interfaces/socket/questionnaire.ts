3c1155d5740a573e7cfe7fa6d541fcbe975cb3c8
Kaburi <specialemail90@gmail.com>
2024-04-30 16:54:31 +0900

    Updated

--- src/interfaces/socket/questionnaire.ts
+++ src/interfaces/socket/questionnaire.ts
@@ -1,5 +1,5 @@
-import { IQuestionnaire } from "health-screening-shared/interfaces";
-import { EmitArgsBase, EmitResultBase } from "./base/result-base";
+import { IQuestionnaire } from "../questionnaire";
+import { EmitArgsBase, EmitResultBase } from "./base";
 
 export interface SaveQuestionnaireArgs extends EmitArgsBase, IQuestionnaire {
   eiAuto: number;
