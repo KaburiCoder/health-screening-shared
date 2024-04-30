a6de558664690230239a7dfaf1981e04597930c9
Kaburi <specialemail90@gmail.com>
2024-04-30 15:56:14 +0900

    Updated

--- src/joi/activity.ts
+++ src/joi/activity.ts
@@ -1,15 +1,21 @@
 import Joi from "joi";
 import { IActivity, IActivityTerm } from "../interfaces";
 
-const ActiveTermSchema = Joi.object<IActivityTerm>({
-  hours: Joi.number().min(1).max(23).required(),
-  minutes: Joi.number().min(0).max(59).default(0).required(),
-});
+// const ActiveTermSchema = Joi.object<IActivityTerm>({
+//   hours: Joi.number().min(1).max(23).required(),
+//   minutes: Joi.number().min(0).max(59).default(0).required(),
+// });
 
 export const ActivitySchema = Joi.object<IActivity>({
   n8_1: Joi.number().min(1).max(7).required(),
-  n8_2: ActiveTermSchema.required(),
+  n8_2: Joi.object<IActivityTerm>({
+    hours: Joi.number().min(1).max(23).required(),
+    minutes: Joi.number().min(0).max(59).default(0).required(),
+  }).required(),
   n9_1: Joi.number().min(1).max(7).required(),
-  n9_2: ActiveTermSchema.required(),
+  n9_2: Joi.object<IActivityTerm>({
+    hours: Joi.number().min(1).max(23).required(),
+    minutes: Joi.number().min(0).max(59).default(0).required(),
+  }).required(),
   n10: Joi.number().required(),
 });
