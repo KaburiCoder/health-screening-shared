1119e9ea71460d54bb0907d304612030f3e2422a
Kaburi <specialemail90@gmail.com>
2024-05-23 12:01:53 +0900

    Updated

--- src/interfaces/socket/reception-patient.ts
+++ src/interfaces/socket/reception-patient.ts
@@ -18,7 +18,7 @@ export interface QuestionnaireKind {
   kind: EQuestionnaireKind;
   name: string;
   written: boolean;
-  add: "elderly" | "depression" | "cognitive"[];
+  addList: "elderly" | "depression" | "cognitive"[];
   addExam: boolean;
   type: EQuestionnaireType;
   code: string;
